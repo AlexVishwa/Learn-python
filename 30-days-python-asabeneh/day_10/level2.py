7a603b36a4637aa0d9fefc8ea574998714fbab41
AlexVishwa <slxv21@hotmail.com>
2025-02-26 14:13:06 +0530

    Level up

---
+++ 30-days-python-asabeneh/day_10/level2.py
@@ -0,0 +1,16 @@
+sum=0
+for i in range(101):
+    sum= sum+ i
+
+print(f"The sum of first 100 numbers is{sum}")
+
+sum=0
+for i in range(101):
+    if(i%2!=0):
+        sum=sum+i
+print(f"The sum of odd numbers{sum}")
+sum=0
+for i in range(101):
+    if(i%2==0):
+        sum=sum+i
+print(f"The sum of even numbers {sum}")
