aa86b5cd872cd5fea4be9493060d06cfcfdeb11b
AlexVishwa <slxv21@hotmail.com>
2025-03-04 23:05:42 +0530

    Removed files by Sahil

--- 30-days-python-asabeneh/2.2.py
+++
@@ -1,5 +0,0 @@
-s = 'sahil'
-print(len(s)) # 5
-
-if len(s) == 5:
-    print('Yes it is a 5.')
--- 30-days-python-asabeneh/2.5.py
+++
@@ -1,4 +0,0 @@
-language = 'Python'
-pto = language[0:6:2] #
-print(pto)              # "Pto"
-print(type(pto) == str) # True
