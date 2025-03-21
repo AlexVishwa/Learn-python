8f4afb65f6982a0d4b52a6389d741a29b3a4ce93
AlexVishwa <slxv21@hotmail.com>
2025-03-21 17:01:57 +0530

    added 2 more functions

--- 30-days-python-asabeneh/day_11/level3.py
+++ 30-days-python-asabeneh/day_11/level3.py
@@ -24,4 +24,29 @@ def check_list(list1):
     else:
         print("list ain't unique")
 
-check_list([1,2,3,3,4,5,6,7,8,9,10])
+# check_list([1,2,3,3,4,5,6,7,8,9,10])
+
+def checkdt(list1):
+    for i in range(len(list1)-1):
+        a=type(list1[i])
+        if(a==type(list1[i+1])):
+            if(i==len(list1)-1):
+                print("All elements are of same type")
+            pass
+        else:
+            print("All elements are not of same type")
+            break
+# checkdt([1,2,3,'alex'])
+    
+def varname():
+    var = input("Enter the variable name:\n")
+    if var[0].isnumeric():  # Check if the first character is a number
+        print("Variable name is invalid")
+        return
+    for char in var:  # Iterate through each character in the variable name
+        if not (char.isalnum() or char == '_'):  # Only alphanumeric and underscore are valid
+            print("Variable name is invalid")
+            return
+    print("Variable name is valid")
+
+varname()
