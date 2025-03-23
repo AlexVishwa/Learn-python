b7a7029d9d7d9c45fae5ad2d3485e6687aae40b4
AlexVishwa <slxv21@hotmail.com>
2025-03-24 05:18:38 +0530

    level1 completed

--- 30-days-python-asabeneh/day_12/level1.py
+++ 30-days-python-asabeneh/day_12/level1.py
@@ -36,11 +36,15 @@ def a():
 # print(a())
 
 listOfChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-a,b=map(int, input("Enter the length and number of Ids").split())
-r=''
-for i in range(b):
-    for j in range(a):
-        r+=str(listOfChars[randint(0,61)])
-    print(r)
-    r=''
+# a,b=map(int, input("Enter the length and number of Ids").split())
+# r=''
+# for i in range(b):
+#     for j in range(a):
+#         r+=str(listOfChars[randint(0,61)])
+#     print(r)
+#     r=''
 # print(r)
+
+def rgbcolorgen():
+    return f'rgb({randint(0,255),randint(0,255),randint(0,255)})'
+print(rgbcolorgen())
