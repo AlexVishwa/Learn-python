1120b15967872cd6fb8fc771bd6128f0c750ed48
AlexVishwa <slxv21@hotmail.com>
2025-03-25 23:54:39 +0530

    Time is power

---
+++ 30-days-python-asabeneh/day_16/day_time.py
@@ -0,0 +1,40 @@
+from datetime import datetime,time,date,timedelta
+date_string = "5 December, 2019"
+print("date_string =", date_string)
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print("a =", a)
+# time(hour, minute and second)
+b = time(10, 30, 50)
+print("b =", b)
+# time(hour, minute and second)
+c = time(hour=10, minute=30, second=50)
+print("c =", c)
+# time(hour, minute, second, microsecond)
+d = time(10, 30, 50, 200555)
+print("d =", d)
+
+# Jan Feb Mar Apr Jun Jul Aug Sep Oct Nov Dec
+# 31 28,29 31 30 31 30 31 31 30 31 30 31 days
+# 52 weeks
+# 365, 366 days 12 months
+# 52,53 Sat,Sun,Mon,Tue,Wed,Thu,Fri
+# 7 days in a week
+today = date(year=2019, month=12, day=5)
+new_year = date(year=2020, month=1, day=1)
+time_left_for_newyear = new_year - today
+# Time left for new year:  27 days, 0:00:00
+print('Time left for new year: ', time_left_for_newyear)
+
+t1 = datetime(year = 2019, month = 12, day = 5, hour = 0, minute = 59, second = 0)
+t2 = datetime(year = 2020, month = 1, day = 1, hour = 0, minute = 0, second = 0)
+diff = t2 - t1
+print('Time left for new year:', diff) # Time left for new year: 26 days, 23: 01: 00
+
+t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
+t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
+t3 = t1 - t2
+print("t3 =", t3)
---
+++ 30-days-python-asabeneh/day_16/level1.py
@@ -0,0 +1,72 @@
+from datetime import datetime,date
+
+# now = datetime.now()
+# print(now)                      # 2021-07-08 07:34:46.549883
+# day = now.day                   # 8
+# month = now.month               # 7
+# year = now.year                 # 2021
+# hour = now.hour                 # 7
+# minute = now.minute             # 38
+# second = now.second
+# timestamp = now.timestamp()
+# print(day, month, year, hour, minute)
+# print('timestamp', timestamp)
+# print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
+
+# new_year = datetime(2020, 1, 1)
+# print(new_year)      # 2020-01-01 00:00:00
+# day = new_year.day
+# month = new_year.month
+# year = new_year.year
+# hour = new_year.hour
+# minute = new_year.minute
+# second = new_year.second
+# print(day, month, year, hour, minute) #1 1 2020 0 0
+# print(f'{day}/{month}/{year}, {hour}:{minute}')  # 1/1/2020, 0:0
+
+#? STRFTIME
+# # current date and time
+# now = datetime.now()
+# t = now.strftime("%H:%M:%S")
+# print("time:", t)
+# time_one = now.strftime("%m/%d/%Y, %H:%M:%S")
+# # mm/dd/YY H:M:S format
+# print("time one:", time_one)
+# time_two = now.strftime("%d/%m/%Y, %H:%M:%S")
+# # dd/mm/YY H:M:S format
+# print("time two:", time_two)
+
+#* STRPTIME
+
+now=datetime.now()
+# print(now)
+# day=now.day
+# month=now.month
+# second=now.second
+# year=now.year
+# hour=now.hour
+# minute=now.minute
+# timestamp=now.timestamp()
+# print('timestamp:',timestamp)
+# print(f'{day}/{month}/{year}, {hour}:{minute}:{second}')
+# # %m/%d/%Y, %H:%M:%S
+# date_object='December/5/2019, 20:25:10'
+# date_string=now.strftime('%B/%d/%Y, %H:%M:%S')
+# print(date_string)
+
+date_string='5 December, 2019'
+print("date_string =", date_string)
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+time_two = date_object.strftime("%d/%m/%Y, %H:%M:%S")
+print(time_two)
+
+# t1=date(year="2025",month="3",day="25")
+# t2=date(year="2026",month="1",day="1")
+# print(t2-t1)
+# TypeError: 'str' object cannot be interpreted as an integer
+t1=date(year=2025,month=3,day=25)
+t2=date(year=2026,month=1,day=1)
+# print(t2-t1)
+t3=date(year=1970,month=1,day=1)
+print(t3-t1)
